369b0e6f6c8b4aabde80268fea0f43a87ade4145
noahlauper <noahlauper@gmail.com>
2025-01-18 20:14:24 +0100

    add config server

---
+++ config-server/src/test/java/ch/bbw/snusShop/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.snusShop.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
