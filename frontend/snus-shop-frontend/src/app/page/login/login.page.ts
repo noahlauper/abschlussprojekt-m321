6f9a58c5062f5ff751936525d3dd27a6cfd16a30
noahlauper <noahlauper@gmail.com>
2025-01-18 21:23:23 +0100

    add login html + ts

--- frontend/snus-shop-frontend/src/app/page/login/login.page.ts
+++ frontend/snus-shop-frontend/src/app/page/login/login.page.ts
@@ -1,19 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import {IonicModule} from '@ionic/angular';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {LoginRequest} from '../../model/LoginRequest';
+import {AuthService} from '../../services/auth/auth.service';
+import {AuthResponse} from '../../model/AuthResponse';
+import {Router} from '@angular/router';
+import { ToastService } from 'src/app/services/toast/toast.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   imports: [
-    IonicModule
+    IonicModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  loginRequest: LoginRequest;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toastService: ToastService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [''],
+    })
+    this.loginRequest = new LoginRequest();
+  }
 
   ngOnInit() {
   }
 
+  loginUser(){
+    this.loginRequest.email = this.loginForm.value.email;
+    this.loginRequest.password = this.loginForm.value.password;
+    this.authService.login(this.loginRequest).subscribe({
+      next: (response: AuthResponse) => {
+        localStorage.setItem('token', response.jwtToken)
+        this.router.navigate(['home']).then()
+
+      },
+      error: err => {
+        this.toastService.createToast('Falscher benutzername oder passwort', 'warning', 2000);
+      }
+    })
+  }
+
+  redirectToRegister() {
+    this.router.navigate(['register']);
+  }
 }
